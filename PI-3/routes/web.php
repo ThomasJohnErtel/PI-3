1ce510530a2458996e9b51c7bcd911f50181beda
Thomas John Ertel <thomasjohnertel@gmail.com>
2024-04-23 22:09:32 -0300

    Update web.php

--- PI-3/routes/web.php
+++ PI-3/routes/web.php
@@ -1,7 +1,26 @@
 <?php
-
+ 
+use App\Http\Controllers\ProfileController;
+use App\Models\CategoriaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\Controller;
+ 
+Route::get('/produto', [ProdutoController::class, 'index'])->name('produto.index');
+Route::get('/categoria',[CategoriaController::class, 'index'])->name('categoria.index');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
