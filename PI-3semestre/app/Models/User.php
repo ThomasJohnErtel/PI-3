f65c786fa6a60328fc3b785b711a5b11f6841949
Thomas John Ertel <thomasjohnertel@gmail.com>
2024-05-07 21:20:59 -0300

    PI Com Laravel das aulas
    Aplicação de laravel com os conteúdos das aulas, sem modificações adicionais

--- PI-3semestre/app/Models/User.php
+++ PI-3semestre/app/Models/User.php
@@ -10,38 +10,18 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    protected $table = "USUARIO";
+    protected $primaryKey = "USUARIO_ID";
+    public $timestamps = false;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'USUARIO_NOME',
+        'USUARIO_EMAIL',
+        'USUARIO_SENHA',
+        'USUARIO_CPF'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'USUARIO_SENHA'
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
